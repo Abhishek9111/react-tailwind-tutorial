a4c9bb98bd75231c91113a5d383b0997dc3af135
Abhishek Kapoor <42829419+Abhishek9111@users.noreply.github.com>
2024-05-12 08:51:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-end">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
